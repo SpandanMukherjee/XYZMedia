d6083223453a70b13d47c2c4051281d87848a85b
S1eZARsalad <atmadiplahiri@gmail.com>
2025-04-25 00:59:35 +0530

    Template changes 1

--- XYZMedia/XYZMedia/settings.py
+++ XYZMedia/XYZMedia/settings.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 LOGIN_URL = '/accounts/auth/login/'
-LOGIN_REDIRECT_URL = '/dashboard/'
+LOGIN_REDIRECT_URL = '/'
 
 
 # Default primary key field type
