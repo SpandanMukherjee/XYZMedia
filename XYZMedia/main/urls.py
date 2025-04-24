db25683ba594b927edf616820313bcadf78c3913
SpandanMukherjee <spandan.mukherjee428@gmail.com>
2025-04-24 22:13:43 +0530

    fix silly mistake

--- XYZMedia/main/urls.py
+++ XYZMedia/main/urls.py
@@ -5,8 +5,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.redirect_to_dashboard, name="redirect_to_dashboard"),
-    path('admin_dashboard', views.admin_dashboard, name="admin_dashboard"),
-    path('employee_dashboard', views.employee_dashboard, name="employee_dashboard"),
-    path('freelancer_dashboard', views.freelancer_dashboard, name="freelancer_dashboard"),
+    path('admin_dashboard/', views.admin_dashboard, name="admin_dashboard"),
+    path('employee_dashboard/', views.employee_dashboard, name="employee_dashboard"),
+    path('freelancer_dashboard/', views.freelancer_dashboard, name="freelancer_dashboard"),
     path('logout/', views.logout_user, name="logout")
 ]
