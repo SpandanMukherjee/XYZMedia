e09c7fa2eace2f26ece4f90a69157df4b61ec624
S1eZARsalad <atmadiplahiri@gmail.com>
2025-04-24 14:00:17 +0530

    Logout

--- XYZMedia/main/views.py
+++ XYZMedia/main/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 @login_required
 def dashboard(response):
     return render(response, 'dashboard.html', {})
+
+@login_required
+def logout_user(response):
+    logout(response)
+    return redirect('/')
