ab033c8e518e9e448aa75b16a074cb0e1d326412
SpandanMukherjee <spandan.mukherjee428@gmail.com>
2025-04-26 07:48:36 +0530

    logoandstatics

--- XYZMedia/XYZMedia/settings.py
+++ XYZMedia/XYZMedia/settings.py
@@ -134,6 +134,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_URL = '/accounts/auth/login/'
 LOGIN_REDIRECT_URL = '/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
